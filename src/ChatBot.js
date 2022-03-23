5209c5c8078658cf1bf2dc06baae9a0b0b5fff2b
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-03-23 12:28:39 +0600

    chat

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -5,14 +5,14 @@ const ChatBot = ({ show, setShow }) => {
   return (
     <div>
       <Modal show={show} dialogClassName="chatBot">
-        <Modal.Header style={{backgroundColor:'#005247'}} closeButton onHide={() => setShow(false)}>
+        <Modal.Header style={{backgroundColor:'#005247',height:'70px'}} closeButton onHide={() => setShow(false)}>
           <Modal.Title >
           <div className="d-flex justify-content-center align-items-center">  <img width={50} src=" https://i.ibb.co/Jt3dydN/chat2.png" alt="support"/>
             <h4 className="text-white ms-2">Support</h4></div>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <iframe title='bot' src='https://webchat.botframework.com/embed/NSTUNow101?s=S70J09r2a-0.-Twl19Ccpvh9CSjA8G0_8nDHmGqu144-874ONSpE6XQ'
+        <iframe title='bot' src='https://webchat.botframework.com/embed/NSTU101Bot?s=daM8ifciiUc.nCu2g9T7brldHpzmVYImmbwfoG56hAH-BXon1qbvf8Y'
             style={{
               minWidth: " 350px",
               width: "100%",
